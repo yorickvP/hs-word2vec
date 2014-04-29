7cb3d885a3695be7a3f98cc1ea248e9fef4c8185
Yorick van Pelt <yorickvanpelt@gmail.com>
2014-04-29 21:19:18 +0200

    add a slightly better extraction for the corpus

--- src/makeCorpus.hs
+++ src/makeCorpus.hs
@@ -19,14 +19,19 @@ processFile x = do
 	case code of
 		ExitSuccess -> do
 			let (|>) = flip ($)
-			let l = lines out |> map strip |> filter usefulline |> filter usefulsentence |>
-					map removeref |> (map $ filter usefulchar) |> join " " |> split "." |>
-					map strip |> filter usefulsentence |> unlines
+			let l = lines out |> map strip |> filterN 1 usefulline |> filter usefulsentence |>
+					map removeref |> (map $ filter usefulchar) |>
+					-- todo: use tokenize.pl
+					join " " |> split "." |>
+					map strip |> filter usefulsentence |> 
+					map tokenWords |>
+					unlines
 			appendFile "corpus.txt" l
 		_ -> return ()
 	where
-		usefulline [] = False
-		usefulline x  = endswith "." x || length x > 10
+		usefulline _ [] _        = False
+		usefulline (a:_) x (b:_) = (length a > 20) || (length x > 20)
+		usefulline _ x _         = endswith "." x || length x > 10
 		usefulchar x  = isLetter x || isNumber x || x == '.' || x == ' '
 		stringhasletter x = isJust $ find (isLetter) x
 		-- strip all of the [x] things, if they have no letters
@@ -39,7 +44,15 @@ processFile x = do
 		removeref  (a:b) = a : removeref b
 		usefulsentence [] = False
 		usefulsentence x = (length (filter (isUpper . (!! 0)) words)) * 2 < (length words)
+			&& length x > 15
+			&& (sum $ map length words) `div` (length words) < length x `div` 2
 			where words = filter (not . null) $ splitWs x
+		tokenWords line = join " " $ map tokenWord $ words line
+		tokenWord x
+			| any isSymbol x      = "<symbol>"
+			| any isNumber x      = "<number>"
+			| not (all isAscii x) = "<nonascii>"
+			| otherwise           = map toLower x
 main :: IO ()
 main = do
 	fexist <- doesFileExist "corpus.txt"
@@ -47,3 +60,15 @@ main = do
 	pdfs <- getDirectoryContents "pdfs"
 	forM_ (map ("pdfs/" ++) pdfs) processFile
 
+lastN :: Int -> [a] -> [a]
+lastN len arr = drop ((length arr) - len) arr
+-- iterate over an n*2 window around the element and filter based on that
+filterN :: Int -> ([a] -> a -> [a] -> Bool) -> [a] -> [a]
+filterN c pred (a:al)   = filterN' c [] a al pred
+filterN c pred []       = []
+filterN' c before x [] pred
+  | pred before x []    = [x]
+  | otherwise           = []
+filterN' c before x after@(n:an) pred
+  | pred before x after = x : filterN' c ((lastN (c-1) before)++[x]) n an pred
+  | otherwise           =     filterN' c ((lastN (c-1) before)++[x]) n an pred
