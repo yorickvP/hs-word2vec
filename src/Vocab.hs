31b8be5445562e79b8d1b585e7e03c50312578b5
Yorick van Pelt <yorickvanpelt@gmail.com>
2014-05-16 00:18:56 +0200

    split off neural net and change corpus stuff into vocabulary

---
+++ src/Vocab.hs
@@ -0,0 +1,167 @@
+{-# LANGUAGE BangPatterns #-}
+module Vocab (
+    countWordFreqs
+  , lineArr
+  , doIteration
+  , makeVocab
+  , uniqueWords
+  , findIndex
+  , Vocab
+)
+where
+
+import System.Random
+import System.Random.Shuffle
+import Control.Monad (liftM, foldM)
+import Data.Maybe (isJust)
+import Control.Monad.Supply
+import Control.Monad.Random
+import Data.Array.IArray
+import Debug.Trace
+
+import Data.Foldable (concatMap, fold)
+import Data.List (foldl', mapAccumL)
+import qualified Data.Traversable as T
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.UTF8 as UTF8
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IM
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
+
+{-
+The corpus file is stored as a text file, where each line is a sentence.
+In order to train the neural net, we need to be able to iterate over every word,
+and train the neural net for every word in the context
+(where we sample the words closer to the source word more often).
+
+Words that occur frequently are skipped over some of the time, but the n words before
+that word are looked at instead.
+
+The strategy applied here is that we take the input lines, and convert them
+into a datastructure, mapping every word to an index, and storing the frequency
+of every word.
+
+After the neural net is done training, its output vectors are paired to the words
+and written out to a file for gnuplot to plot.
+
+-}
+{-
+Let's specify a datastructure for the word counts and indices. We can't give them
+indices immediately, because words that occur less than n times (with n typically
+around 2-5) have to be filtered out first.
+This is why we have a datastructure storing just the word counts, and one that also
+countains a label.
+-}
+type WordCounts   = HashMap B.ByteString Int
+type WordIdCounts = HashMap B.ByteString (Int, Int)
+{-
+Then, we need a datastructure to map the indices back to words,
+so that we can make nice graphs later on.
+-}
+type IndexWordMap = IntMap B.ByteString
+{-
+Now that we have these structures, let's put them into a datatype for easy use.
+Let's also include the training context size (how many words to look around every word),
+because it's really a property used to generate and use the corpus.
+Another thing that is useful to keep track of is the number of words, because it's O(n)
+to find it from the datastructures
+-}
+
+data Vocab = Vocab { wordCounts :: !WordIdCounts
+                   , wordCount  :: !Int         -- total word count, sum of wordCounts
+                   , vocabWords :: !IndexWordMap }
+
+
+
+iterateWordContexts :: (RandomGen g) =>
+	[Array Int B.ByteString] -> Int -> (B.ByteString -> Maybe b) -> (B.ByteString -> Rand g Bool) -> (b -> a -> B.ByteString -> a) -> a -> Rand g a
+iterateWordContexts (arr:xs) ctx primary_filter filt folder start = do
+	-- iterate over array
+	r <- getRandomR (1, ctx)
+	result <- foldM (idxfold r) start $ indices arr
+	-- get result
+	iterateWordContexts xs ctx primary_filter filt folder result
+	where
+		-- need scoped type variables
+		-- idxfold :: (RandomGen g) => a -> Int -> Rand g a
+		idxfold lookaround strt idx = do
+			let (lower, upper) = bounds arr
+			case primary_filter (arr ! idx) of
+				Nothing   -> return strt
+				Just word -> do
+					before <- wrds (enumFromThenTo (idx - 1) (idx - 2) lower)
+					after  <- wrds (enumFromThenTo (idx + 1) (idx + 2) upper)
+					let strt' = foldl' (folder word) strt before
+					return    $ foldl' (folder word) strt' after
+			where
+				--wrds :: (RandomGen g) => [Int] -> Rand g [B.ByteString]
+				wrds x = takeNfiltM x (\i -> do
+						let s = arr ! i
+						out <- filt s
+						return (if out then Just s else Nothing)) lookaround
+iterateWordContexts [] _ _ _ _ start = return start
+
+-- helper function, something like (take n $ catMaybes $ map pred xs), but monadically
+-- without evaluating any unneeded results, like mapM would.
+takeNfiltM :: (Monad m) => [a] -> (a -> m (Maybe b)) -> Int -> m [b]
+takeNfiltM [] _ _ = return []
+takeNfiltM  _ _ 0 = return []
+takeNfiltM (x:xs) pred n = do
+	x' <- pred x
+	case x' of
+		Nothing  -> takeNfiltM xs pred n
+		Just val -> liftM (val :) $ takeNfiltM xs pred (n - 1)
+
+
+countWordFreqs :: [B.ByteString] -> WordCounts
+countWordFreqs = foldl' (flip (flip (HM.insertWith (+)) 1)) HM.empty
+
+labelWords :: WordCounts -> WordIdCounts
+labelWords arr = evalSupply (T.mapM (\x -> do { i <- supply; return (i, x) }) arr) [0..]
+-- the int is the amount of times a word needs to occur before it's included
+makeVocab :: WordCounts -> Int -> Vocab
+makeVocab counts thresh = Vocab labelled (HM.foldl' (+) 0 newcounts) indices
+	where
+		!newcounts = HM.filter (>= thresh) counts
+		!labelled = labelWords newcounts
+		!indices = HM.foldlWithKey' (\o k (i, _) -> IM.insert i k o) IM.empty labelled
+
+uniqueWords :: Vocab -> Int
+uniqueWords = HM.size . wordCounts
+
+lineArr :: B.ByteString -> [Array Int B.ByteString]
+lineArr = map (toArray . C8.words) . UTF8.lines
+	where toArray x = listArray (0, length x - 1) x
+
+
+hasWord :: Vocab -> B.ByteString -> Bool
+hasWord (Vocab tr _ _) = isJust . (flip HM.lookup) tr
+
+wordIdx :: Vocab -> B.ByteString -> Int
+wordIdx (Vocab v _ _) = fst . (v HM.!)
+
+safeWordIdx :: Vocab -> B.ByteString -> Maybe Int
+safeWordIdx (Vocab v _ _) = fmap fst . (flip HM.lookup) v
+
+-- exported
+findIndex :: Vocab -> Int -> B.ByteString
+findIndex vocab str = (vocabWords vocab) IM.! str
+
+wordFreq :: Vocab -> B.ByteString -> Float
+wordFreq (Vocab tr total _) x = ((fromIntegral count) / (fromIntegral total))
+	where (_, count) = tr HM.! x
+
+subsample :: (RandomGen g) => Vocab -> B.ByteString -> Rand g Bool
+subsample vocab x = if not $ hasWord vocab x then return False else do
+	r <- getRandom
+	let freq = wordFreq vocab x
+	let prob = 1 - (sqrt (1e-5 / freq))
+	return $ prob >= (r :: Float)
+doIteration :: (RandomGen g) => Vocab -> B.ByteString -> Int -> (a -> (Int, Int) -> a) -> a -> Rand g a
+doIteration vocab str ctx folder net = iterateWordContexts (lineArr str) ctx (safeWordIdx vocab) filt train net
+	where
+		filt = subsample vocab
+		train a net b = folder net (a, wordIdx vocab b)
--- src/classify.hs
+++ src/classify.hs
@@ -24,117 +24,54 @@ import Control.Monad.Random (Rand, evalRandIO)
 import Debug.Trace
 import Text.Printf
 import Data.String.Utils
-import Control.DeepSeq
 
 import Data.List (foldl')
+import Data.Array.IArray
 import qualified Data.Packed.Matrix as Mt
 import qualified Data.Packed.Vector as Mt
 import qualified Numeric.LinearAlgebra as Mt
 import qualified Numeric.LinearAlgebra.Algorithms as Mt
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IM
-import System.Cmd (rawSystem)
+import System.Process (rawSystem)
 import System.Exit (ExitCode (ExitSuccess))
+import qualified Data.ByteString as B
 import qualified Data.ByteString.UTF8 as UTF8
-import qualified Corpus as Corpus
+import qualified Data.ByteString.Char8 as C8
+import qualified Vocab as Vocab
+import qualified NeuralNet as NN
+import Util
 
 type DVec = Mt.Vector Double
 type DMat = Mt.Matrix Double
--- output is a V * feat matrix
-runNetwork :: DVec -> DMat -> DVec
-runNetwork feature output = feature `Mt.vXm` output
 
--- exp(a_i) / sum(k)(exp(a_k))
-softmax :: DVec -> Int -> Double
-softmax a i = (expa Mt.@> i) / (Mt.sumElements expa)
-	where expa = Mt.mapVector exp $ a
--- derivative over q_k: = (d(k,i) - s(a,k)) * s(a,i)
--- k and i can be swapped
--- from wikipedia/Softmax_function
-softmax' :: DVec -> Int -> Int -> Double
-softmax' a i k = ((delta k i) - softmax a k) * softmax a i
-	where delta a b = if a == b then 1 else 0
-logsoftmax :: DVec -> Int -> Double
-logsoftmax a i = if (0 == softmax a i) then error "log of zero" else log $ softmax a i
--- derivative over q_k: s'(a, i, k) / s(a, i)
--- k and i can *not* be swapped
-logsoftmax' :: DVec -> Int -> Int -> Double
-logsoftmax' a i k = (softmax' a i k) / (softmax a i)
-
-imap' :: (Int -> a -> b) -> Int -> [a] -> [b]
-imap' _ _ []     = []
-imap' f i (x:xs) = f i x : imap' f (i + 1) xs
-imap = flip imap' 0
-
-runWord :: DVec -> DMat -> Int -> (DVec, DMat)
-runWord feature output expected = (newfeat, newout)
-	where
-		out = runNetwork feature output
-		softerr = 0 - (logsoftmax out expected)
-		-- calculate the delta for the output layer
-		-- the softmax can be seen as another layer on top of it with fixed weight 1
-		-- this means this is also the modified error of the output layer
-		-- delta_k = Err_k * g'(in_k)
-		moderr = Mt.mapVector (* softerr) $ Mt.buildVector (Mt.dim out) (logsoftmax' out expected)
-		-- calculate the weight adjustment between the output and projection
-		-- W_(j,k) := W_(j,k) + alpha * a_j * delta_k
-		newout = Mt.fromColumns $ imap processout $ Mt.toColumns output
-		processout k feat = Mt.zipVectorWith (adjustprojoutcon (moderr Mt.@> k)) feature feat
-		adjustprojoutcon d_k activation weight = weight + rate * d_k * activation
-		-- calculate the propagation deltas for the projection layer
-		delta_j = output `Mt.mXv` moderr
-		-- calculate the new weights for the feature vector
-		newfeat = check_size feature delta_j $ Mt.zipVectorWith (\w d -> w + rate * d) feature delta_j
-		--output !! k ! j + rate * (feature ! j) * (moderr ! k)
-		rate = 0.005 :: Double
-		traceShowId a = trace (show a) a
-		check_size a b x = if (Mt.dim a /= Mt.dim b) then error "size doesn't match" else x
-
-
-randomNetwork :: Int -> Int -> IO (DMat, DMat)
-randomNetwork vocab dimen = do
-	seeda <- randomIO :: IO Int
-	seedb <- randomIO :: IO Int
-	let a = Mt.randomVector seeda Mt.Uniform (vocab * dimen)
-	let b = Mt.randomVector seedb Mt.Uniform (vocab * dimen)
-	return (Mt.reshape dimen a, Mt.reshape vocab b)
-
-
-runWords :: (RandomGen g) => Corpus.Corpus -> IntMap DVec -> DMat -> Rand g (IntMap DVec, DMat)
-runWords words feat out = do
-	pairs <- Corpus.wordPairs words
-	return $! foldl' runWordPair (feat, out) pairs
-	where
-		runWordPair (f, o) (a, b) = (IM.insert a thisf f, newo)
-			where (thisf, newo) = runWord (f IM.! a) o b
-
-runAllWords :: Corpus.Corpus -> Int -> IO ()
-runAllWords wrds dimens = do
-	(feat, out) <- randomNetwork (Corpus.numWords wrds) dimens
-	let ft = IM.fromAscList $ imap (,) (Mt.toRows feat)
-	(ft_, ot_) <- iter (ft, out) 0
+runAllWords :: Vocab.Vocab -> B.ByteString -> Int -> IO ()
+runAllWords vocab content dimens = do
+	net  <- NN.randomNetwork (Vocab.uniqueWords vocab) dimens
+	net_ <- iter vocab net 0
 	putStrLn $ "complete: "
 	--putStrLn $ "feature: " ++ (show $ IM.toList ft_)
 	--putStrLn $ "output:  " ++ (show $ ot_)
-	let v = runNetwork (ft_ IM.! 2) ot_
-	let outs = Mt.mapVectorWithIndex (\i _ -> softmax v i) v
+	let v = NN.runNetwork net 2
+	--let outs = Mt.mapVectorWithIndex (\i _ -> softmax v i) v
 	--putStrLn $ "full softmax output: " ++ (show $ map (printf "%.2f" :: Double->String) $ Mt.toList outs)
-	a <- plot $ imap (\i x -> (UTF8.toString $ Corpus.findIndex wrds i, x Mt.@> 0, x Mt.@> 1) ) $ pca 2 ft_
+	let features = NN.featureVecArray net_
+	a <- plot $ imap (\i x -> (UTF8.toString $ Vocab.findIndex vocab i, x Mt.@> 0, x Mt.@> 1) ) $ pca 2 features
 	return ()
-	where iter (ft, ot) x = do
-		(ft2, ot2) <- evalRandIO $ runWords wrds ft ot
-		putStrLn $ "iteration " ++ (show x) ++ " complete " ++ (show $ IM.size $ ft2)
+	where iter vocab net x = do
+		net2 <- evalRandIO $ Vocab.doIteration vocab content 5 NN.runWord net
+		putStrLn $ "iteration " ++ (show x) ++ " complete "  ++ (show $ NN.forceEval net2)
 		--let v = runNetwork (ft2 IM.! 0) ot2
 		--et outs = Mt.mapVectorWithIndex (\i _ -> softmax v i) v
 		--putStrLn $ "full softmax output: " ++ (show $ map (printf "%.2f" :: Double->String) $ Mt.toList outs)
 		--putStrLn $ "network output     : " ++ (show $ map (printf "%.2f" :: Double->String) $ Mt.toList v)
-		if x < 2 then iter (ft2, ot2) (x + 1) else return (ft2, ot2)
+		if x < 3 then iter vocab net2 (x + 1) else return net2
 
 main = do
-	crps <- Corpus.getFullCorpus
-	putStrLn $ "corpus loading complete " ++ (show $ Corpus.numWords crps)
-	a <- evalRandIO $ Corpus.wordPairs crps
-	runAllWords crps 110
+	crps <- B.readFile "corpus.txt"
+	let vocab = Vocab.makeVocab (Vocab.countWordFreqs $ C8.words crps) 2
+	putStrLn $ "Vocab loading complete " ++ (show $ Vocab.uniqueWords vocab)
+	runAllWords vocab crps 110
 	return ()
 
 normalize_mean :: [Mt.Vector Double] -> [Mt.Vector Double]
@@ -148,13 +85,11 @@ eigenvectors :: Mt.Matrix Double -> [Mt.Vector Double]
 eigenvectors x = Mt.toColumns x where
 	(u, s, v) = Mt.svd x
 
-pca :: Int -> IntMap DVec -> [DVec]
+pca :: Int -> [DVec] -> [DVec]
 pca dims x = map (`Mt.vXm` base) dataset
 	where
 		base = Mt.fromColumns $ take dims $ eigenvectors $ covariance_matrix $ dataset
-		dataset = normalize_mean $ toVecArray x
-		toVecArray :: IntMap DVec -> [DVec]
-		toVecArray x = map snd $ IM.toAscList x
+		dataset = normalize_mean $ x
 
 -- loosely based on http://hackage.haskell.org/package/easyplot-1.0/docs/src/Graphics-EasyPlot.html#Plot
 plot :: (Show a, Num a) => [(String, a, a)] -> IO Bool
