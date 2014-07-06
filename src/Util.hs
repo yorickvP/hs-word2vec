b00f8c18c0ebe88e19598965b9a15c13ca16635c
Yorick van Pelt <yorickvanpelt@gmail.com>
2014-07-06 18:50:19 +0200

    revert earlier commit, move vector file reading back

--- src/Util.hs
+++ src/Util.hs
@@ -2,20 +2,12 @@ module Util (
 	imap
   , plot
   , plotLine
-  , readVectorsFile
 )
 where
 
 import System.Process (rawSystem)
 import System.Exit (ExitCode (ExitSuccess))
 import Data.String.Utils
-import qualified Data.ByteString.Lazy as BS
-import qualified Data.ByteString.Lazy.Char8 as BC8
-import Data.Binary.Get
-import Data.Binary.IEEE754
-import Control.Monad hiding (join)
-import GHC.Float
-import qualified Data.Packed.Vector as Mt
 
 
 imap' :: (Int -> a -> b) -> Int -> [a] -> [b]
@@ -54,45 +46,3 @@ plotLine points = do
 				"set output \"line.png\"",
 				"plot \"" ++ filename ++ "\" using 1:2 with linespoints title \"\""]]
 		filename = "plot_line.dat"
-maybeTake :: Maybe Int -> [a] -> [a]
-maybeTake Nothing a = a
-maybeTake (Just x) a = take x a
-
-readUp :: Char -> BS.ByteString -> (BS.ByteString, BS.ByteString)
-readUp chr str = let (a, b) = BC8.break (== chr) str
-				 in (a, BS.drop 1 b)
-
-readVectorsFile :: String -> Bool -> Maybe Int -> IO [(BS.ByteString, Mt.Vector Double)]
-readVectorsFile filename binary limit = do
-	filecontent <- BS.readFile $ filename
-	let (firstLine, vecs) = readUp '\n' filecontent
-	let [_, Just (numVecs, _)] = fmap BC8.readInt $ BC8.words firstLine
-	putStrLn $ "reading " ++ (show numVecs) ++ "-dimensional vectors"
-	return $ if binary then
-		let vecpairs = maybeTake limit $ iterate numVecs vecs
-		    fieldlist = map (Mt.fromList . snd) vecpairs
-		in  zip (map fst vecpairs) fieldlist -- add the words back on
-	else
-		let cleanlines = maybeTake limit $ BC8.lines vecs
-		    fieldlist = map readFields cleanlines
-		in
-			zip (map (head . BC8.words) cleanlines) fieldlist
-	where
-		readFields x = Mt.fromList (map (read . BC8.unpack) $ tail $ BC8.words x :: [Double])
-		iterate :: Int -> BS.ByteString -> [(BS.ByteString, [Double])]
-		iterate noVecs start
-			| BS.length (BS.take 2 start) < 2 = []
-			| otherwise = let (rest, word, vecs) = parseBinary noVecs start
-						  in  (word, map float2Double vecs) : iterate noVecs rest
-
-
-parseBinary :: Int -> BS.ByteString -> (BS.ByteString, BS.ByteString, [Float])
-parseBinary noVecs startStr = (rest', word, vectors)
-	where
-		-- remove leading \n
-		vecs' = BC8.dropWhile (\a -> ((a == '\n') || (a == ' '))) startStr
-		-- read until space
-		(word, rest) = readUp ' ' vecs'
-		-- read the things
-		(vectors, rest') = runGet parseBin rest
-		parseBin = liftM2 (,) (replicateM noVecs getFloat32le) getRemainingLazyByteString
