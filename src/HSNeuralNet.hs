dd956815fdefc17d5d1cb546f44a80387e7a7dbd
Yorick van Pelt <yorickvanpelt@gmail.com>
2014-07-06 23:07:02 +0200

    clean up and comment HSNeuralNet.hs

--- src/HSNeuralNet.hs
+++ src/HSNeuralNet.hs
@@ -1,7 +1,6 @@
 module HSNeuralNet (
     NeuralNet
   , randomNetwork
-  , featureVecArray
   , runWord
   , getFeat
   , forceEval
@@ -11,11 +10,9 @@ module HSNeuralNet (
 where
 
 import System.Random
-import qualified Data.Packed.Matrix as Mt
-import qualified Data.Packed.Vector as Mt
 import qualified Numeric.LinearAlgebra as Mt
 import Numeric.LinearAlgebra (dot, scale)
-import Data.IntMap.Strict (IntMap, (!), insert, fromAscList, size, toAscList)
+import Data.IntMap.Strict (IntMap, (!), insert, fromAscList, size)
 import Data.List (foldl')
 import Vocab (WordDesc (..), TrainProgress (..))
 import Control.Monad.Writer
@@ -29,17 +26,8 @@ Let's specify a type to store our entire network in.
 Together with the Vocab, this'll be the state of the entire system.
 -}
 data NeuralNet = NeuralNet !(IntMap Features) !(OutLayer) !Average
-newtype Average = Average (Double, Int)
-calcAvg :: Average -> Double
-calcAvg (Average (total, len)) = total / (fromIntegral len)
-addAvg :: Average -> Double -> Int -> Average
-addAvg (Average (total, len)) subtot sublen = Average (total + subtot, len + sublen)
-cleanAvg :: Average
-cleanAvg = Average (0, 0)
-avgSize :: Average -> Int
-avgSize (Average (_, len)) = len
-type StatusUpdate = (Double, TrainProgress, Average)
-type StatusWriter = Writer [StatusUpdate]
+
+
 -- furthermore, we're going to need functions for common operations
 -- (exported) getFeat gets a feature vector for a specific word index
 -- this is also used to output the word data
@@ -51,53 +39,7 @@ updateNet :: NeuralNet -> Int -> Features -> (OutLayer) -> Average -> NeuralNet
 updateNet (NeuralNet x _ _) idx val out avg =
 	NeuralNet (insert idx val x) out avg
 
-sigmoid :: Double -> Double
-sigmoid x = 1.0 / (1.0 + (exp (- x)))
-
--- we are optimizing
--- sum(words) sum(context) log product(point,code)(sigmoid (code?1:-1) * (l1 . point))
--- using exp-sum-log instead of product:
--- sum(words) sum(context) log exp sum(point,code)(log sigmoid (code?1:-1) * (l1 . point))
--- the log/exp cancel out, so we can individually optimize log sigmoid(code?1:-1)*(l1.point)
--- huffman trees are useful because they minimize the calculations
--- (hierarchical softmax works with any kind of binary tree). huffman trees assign shorter
--- codes to more frequent words, so less calculations needed on a whole.
--- so calculate logsigmoid for every code and point
--- derivative of logsigmoid(1 * x) = 1 - sigmoid(x) (or sigmoid(-x))
--- derivative of logsigmoid(-1 * x) = 0 - sigmoid(x) (or -sigmoid(x))
-(rateMax, rateMin) = (0.025, 0.001) :: (Double, Double)
-rateAdj :: TrainProgress -> Double
-rateAdj (TrainProgress itcount total) =
-	max rateMin $ rateMax * (1.0 - ((fromIntegral itcount) / (1.0 + fromIntegral total)))
-
--- foldl [(Bool, Int)] -> NeuralNet -> (neu1e, NeuralNet)
-singleIter :: Double -> Features -> (DVec, OutLayer, Double) -> (Bool, Int) -> (DVec, OutLayer, Double)
-singleIter rate l1 (neu1e, output, err) (c, p) = (neu1e', output', err + errf)
-	where
-		l2      = output ! p
-		f       = l1 `dot` l2
-		errf    = log $ sigmoid $ (if c then -1.0 else (1.0)) * f -- flip 1 and -1?
-		--   g  = logsigmoid_c'(l1 `dot` l2) * rate
-		g       = (1.0 - (fromIntegral $ fromEnum c) - (sigmoid f)) * rate
-		neu1e'  = neu1e + (g `scale` l2)
-		output' = insert p (l2 + (g `scale` l1)) output
-
--- use a word pair (and label/place in the tree of the word we're looking around)
-runWord :: NeuralNet -> TrainProgress -> (WordDesc, WordDesc) -> StatusWriter NeuralNet
-runWord net@(NeuralNet _ output avg) progress@(TrainProgress itcount _)
-							  (WordDesc _ treepos, WordDesc expected _) = do
-	when ((avgSize avg' > 100) && ((itcount `mod` 10000) == 0)) $ tell [(rate, progress, avg')]
-	return $ updateNet net expected newfeat newout newavg
-	where
-		rate    = rateAdj progress
-		l1      = getFeat net expected
-		neu1e   = Mt.constant 0.0 (Mt.dim l1)
-		(neu1e', newout, toterr) = foldl' (singleIter rate l1) (neu1e, output, 0.0) treepos
-		newfeat = l1 + neu1e'
-		avg'    = addAvg avg toterr (length treepos)
-		newavg  = if (itcount `mod` 10000) == 0 then cleanAvg else avg' -- trace calcAvg avg'
-
-
+-- generate a starting network, ft = rand(0,1) / dimen. ot = 0.
 randomNetwork :: Int -> Int -> IO NeuralNet
 randomNetwork vocab dimen = do
 	seeda <- randomIO :: IO Int
@@ -109,8 +51,93 @@ randomNetwork vocab dimen = do
 	return $ NeuralNet ft ot cleanAvg
 
 
-featureVecArray :: NeuralNet -> [Features]
-featureVecArray (NeuralNet ft_ _ _) = map snd $ toAscList ft_
-
 forceEval :: NeuralNet -> Int
 forceEval (NeuralNet ft _ _) = size ft
+
+-- A datatype for keeping track of averages (that are still being added to)
+newtype Average = Average (Double, Int)
+
+calcAvg :: Average -> Double
+calcAvg (Average (total, len)) = total / (fromIntegral len)
+addAvg :: Average -> Double -> Int -> Average
+addAvg (Average (total, len)) subtot sublen = Average (total + subtot, len + sublen)
+cleanAvg :: Average
+cleanAvg = Average (0, 0)
+avgSize :: Average -> Int
+avgSize (Average (_, len)) = len
+
+-- and some types for reporting the average error and progress
+type StatusUpdate = (Double, TrainProgress, Average)
+type StatusWriter = Writer [StatusUpdate]
+
+
+-- the sigmoid function
+sigmoid :: Double -> Double
+sigmoid x = 1.0 / (1.0 + (exp (- x)))
+{-
+we are optimizing
+sum(words) sum(context) log product(point,code)(sigmoid (code?1:-1) * (l1 . point))
+using exp-sum-log instead of product:
+sum(words) sum(context) log exp sum(point,code)(log sigmoid (code?1:-1) * (l1 . point))
+the log/exp cancel out, so we can individually optimize log sigmoid(code?1:-1)*(l1.point)
+using Stochastic Gradient Descent
+
+huffman trees are useful because they minimize the calculations
+(hierarchical softmax works with any kind of binary tree). huffman trees assign shorter
+codes to more frequent words, so less calculations needed on a whole.
+
+
+The derivatives of log sigmoid for both codes:
+derivative of logsigmoid(1 * x) = 1 - sigmoid(x) (or sigmoid(-x))
+derivative of logsigmoid(-1 * x) = 0 - sigmoid(x) (or -sigmoid(x))
+-}
+-- Let's define the rate for a particular part of training, to get a decreasing learning rate.
+-- this might be more helpful with a bigger corpus, when there isn't much to be learned towards the end.
+rateMax :: Double
+rateMin :: Double
+(rateMax, rateMin) = (0.025, 0.001)
+rateAdj :: TrainProgress -> Double
+rateAdj (TrainProgress itcount total) =
+	max rateMin $ rateMax * (1.0 - ((fromIntegral itcount) / (1.0 + fromIntegral total)))
+{-
+For every word a: for every some word b around it (this code is in Vocab), lookup a in a binary tree,
+recalculate the learning rate, and perform gradient descent
+on multiply the vector in layer 0(features) corresponding to word b, with each of the
+vectors in layer 1 corresponding to the path to the top the binary tree (taking into consideration
+if something is the left or right child).
+While doing this, keep track of the average gradient descent error, and every 10000 iterations, write a
+progress report.
+The code below is mostly equivalent to the word2vec C code for skipgram hierarchical softmax
+-}
+runWord :: NeuralNet -> TrainProgress -> (WordDesc, WordDesc) -> StatusWriter NeuralNet
+runWord net@(NeuralNet _ output avg) progress@(TrainProgress itcount _)
+							  (WordDesc _ treepos, WordDesc expected _) = do
+	-- write a progress report every 10000 iterations (if we didn't already, this function is
+	-- used more than once per iteration)
+	when ((avgSize avg' > 100) && ((itcount `mod` 10000) == 0)) $ tell [(rate, progress, avg')]
+	return $ updateNet net expected newfeat newout newavg
+	where
+		rate    = rateAdj progress
+		l1      = getFeat net expected
+		neu1e   = Mt.constant 0.0 (Mt.dim l1)
+		(neu1e', newout, toterr) = foldl' (singleIter rate l1) (neu1e, output, 0.0) treepos
+		newfeat = l1 + neu1e'
+		-- keep track of the average error over some iterations
+		avg'    = addAvg avg toterr (length treepos)
+		newavg  = if (itcount `mod` 10000) == 0 then cleanAvg else avg'
+
+-- this function runs for every node between the word and the top of the tree. We update the output
+-- instantly, but sum the feature changes and update that when done (the output wouldn't be practical to
+-- wait with, because it has to update for multiple points)
+singleIter :: Double -> Features -> (DVec, OutLayer, Double) -> (Bool, Int) -> (DVec, OutLayer, Double)
+singleIter rate l1 (neu1e, output, err) (c, p) = (neu1e', output', err + errf)
+	where
+		l2      = output ! p
+		f       = l1 `dot` l2
+		-- calculate the error
+		errf    = log $ sigmoid $ (if c then -1.0 else (1.0)) * f
+		--   g  = logsigmoid_c'(l1 `dot` l2) * rate
+		g       = (1.0 - (fromIntegral $ fromEnum c) - (sigmoid f)) * rate
+		neu1e'  = neu1e + (g `scale` l2)
+		output' = insert p (l2 + (g `scale` l1)) output
+
